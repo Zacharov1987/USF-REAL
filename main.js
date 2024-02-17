af351ff07d84e0a6f027c0fb11626823959ddc09
Zacharov1987 <160329224+Zacharov1987@users.noreply.github.com>
2024-02-18 00:04:49 +0300

    v2

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-import disnake
-
-from disnake.ext import commands
-
 bot = commands.Bot(command_prefix="*", intents=disnake.Intents.all())
-
 
 @bot.slash_command()
 async def test(interaction: disnake.AppCmdInter):
